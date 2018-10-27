3f032f2e04cacb5b2a77b83924957122efdf42f6
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-10-26 21:23:53 -0400

    Robert Hogan. Back end of project is fully completed. All routes are functional with proper authorization, authentication, hashing of passwords. Users are now able to create accounts, create profiles, add education history, work history, comment, like, and delete comments and likes. Plus more.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     Post.findById(req.params.id)
     .then(post => res.json(post))
-    .catch(err => res.status(404).json({nopostfound: 'No post was found with that id'}));
+    .catch(err => res.status(404).json({ nopostfound: 'No post was found with that id' }));
 });
 
 //@route - POST api/posts
@@ -125,7 +125,63 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
             post.save().then(post => res.json(post));
         })
         .catch(err => res.status(404).json({ postnotfound: 'Post was not found' }));
-    })
+    });
+});
+
+//@route - POST api/posts/comment/:id
+//@description - Add a comment to post
+//@access - Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+    //validation 
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check validation
+if (!isValid) {
+    // If any errors, send 400 with errors obj
+    return res.status(400).json(errors);
+}
+
+    Post.findById(req.params.id)
+        .then(post => {
+            const newComment = {
+                text: req.body.text,
+                name: req.body.name, 
+                avatar: req.body.avatar,
+                user: req.user.id
+            }
+
+            // Add to comments array
+            post.comments.unshift(newComment);
+
+            //save
+            post.save().then(post => res.json(post))
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
+//@route - DELETE api/posts/comment/:id/:comment_id
+//@description - Delete a comment from a post
+//@access - Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+    Post.findById(req.params.id)
+        .then(post => {
+            // Check if comment exists
+            if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+                return res.status(404).json({ commentnotexists: 'Comment does not exist' });
+            }
+
+            // Get remove index
+            const removeIndex = post.comments.map(item => item.id.toString())
+            .indexOf(req.params.comment_id);
+
+            //Splice out of the array
+            post.comments.splice(removeIndex, 1);
+
+            post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
 });
 
 module.exports = router;
